9a95cf755263f091431f7866b0baee4492287158
msantero <msantero@montevideo.com.uy>
2021-09-26 22:07:45 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-bvlpsf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-bvlpsf)
---
+++ src/app/usuarios.ts
@@ -0,0 +1,23 @@
+export interface Usuario {
+  id: number;
+  usuario: string;
+  password: string;
+  apiKey: string;
+}
+
+/*
+export const usuarios = [
+  {
+    id: 1,
+    usuario: 'Marcos',
+    password: '123',
+    apiKey: 'dfgdfg'
+  },
+  {
+    id: 1,
+    usuario: 'Pedro',
+    password: '456',
+    apiKey: 'sdfsdffff'
+  }
+];
+*/
